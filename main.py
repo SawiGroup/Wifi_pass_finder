394ce902f590e0459a11f9f6e0262507d4d97889
SawiGroup <163553951+SawiGroup@users.noreply.github.com>
2024-03-26 11:48:32 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import subprocess
+
+def find_wifi_password(network_SSID):
+    results = subprocess.check_output(["netsh", "wlan", "show", "profile"]).decode("utf-8").split("\n")
+    profile_names = [line.split(":")[1][1:-1] for line in results if "All User Profile" in line]
+    password = ""
+    for name in profile_names:
+        profile_info = subprocess.check_output(["netsh", "wlan", "show", "profile", name, "key=clear"]).decode("utf-8").split("\n")
+        for line in profile_info:
+            if "Key Content" in line:
+                password = line.split(":")[1][1:-1]
+                break
+        if name == network_SSID:
+            break
+    return password
